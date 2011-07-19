43d1128036fad878505774db9bd3a5e51c3ba999
bill <bill.mill@gmail.com>
2011-07-19 14:41:26 -0400

    A dead-simple json schema testing tool

---
+++ tester.rb
@@ -0,0 +1,36 @@
+require 'rubygems'
+require 'sinatra'
+require 'erb'
+require 'json-schema'
+
+get '/' do
+  erb :index
+end
+
+post '/test' do
+  begin
+    json = JSON::parse request.body.read
+  rescue JSON::ParserError => err
+    return {"error" => "Invalid JSON"}.to_json
+  end
+
+  schema = json["schema"]
+  sample = json["sample"]
+
+  begin
+    JSON::Validator.validate2(schema, sample)
+  rescue JSON::ValidationError => err
+  rescue Errno::ENOENT => err
+    #If the first argument to validate2 is invalid json, the validator will assume
+    #it's a filename, and go try and load it. Since it probably doesn't exist,
+    #json-schema will throw an Errno::ENOENT
+    err = "Invalid Schema JSON"
+  end
+
+  if err
+    status 400
+    {"error" => err}.to_json
+  else
+    {}.to_json
+  end
+end
